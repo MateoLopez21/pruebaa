818fcfc7200465d6c06e4d29a0c2c75db8ebec24
yesica.giraldo_arbus <yesica.giraldo_arbus@NA-POR0193>
2023-06-07 13:42:45 -0500

    Cambio

--- proyecto/lib/src/main/java/proyecto/tasks/login_Task.java
+++ proyecto/lib/src/main/java/proyecto/tasks/login_Task.java
@@ -2,4 +2,18 @@ package proyecto.tasks;
 
 public class login_Task {
 
+	/* 
+	 * aaaaaaaaaaaaaaaaa bbbbbbbbbbbbbbbbbbbbbbbbbb ccccccccccccccc dddddddddddd eeeeeeeeeeeeeeee ffffffffffffffffff ggggggggggggg hhhhhhhhhhh
+	 * iiiiiiiiiiiiiiiiiiiiiiiiiiiiijjjjjjjjjjjjjjjjjjjjjkkkkkkkkkkkkkkkkkkkkkkkkkkkk llllllllllllllllll mmmmmmmmmmmmnnnnnnnnnnnnnnnnnnnn
+	 * nnnnnnnnnnnoooooooooooooopppppppppppppppppppppqqqqqqqqqqqqqqqqqqqqqqrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
+	 * rrrssssssssssssssssssssssssstttttttttttttttttttttttuuuuuuuuuuuuuuuuuuuuuuuvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvwwwwwwwwwwwwwwwwwwwyyyy
+	 * yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+	 * 
+	 * 
+	 * iuyh
+	 * 
+	 * 
+	 * WQAESRD8765432345678908IUJHNBVCX.LIKJHGF X V
+	 * 
+	 */
 }
